06935ddfbbf06fc69e0bab1a1ccd2e9900644ae7
Jay Dalvi <jaysdalvi05@gmail.com>
2026-07-01 21:04:08 +0530

    fix(backend): correct Mongoose async pre-save password hashing hook

--- express-api/models/User.js
+++ express-api/models/User.js
@@ -28,15 +28,10 @@ const UserSchema = new mongoose.Schema({
 });
 
 // Hash password before saving
-UserSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (err) {
-    next(err);
-  }
+UserSchema.pre('save', async function () {
+  if (!this.isModified('password')) return;
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 // Compare password method
